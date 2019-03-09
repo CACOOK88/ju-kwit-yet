226ad77bc5b218f771b81eef3912a83b457948e7
Chad Cook <chadcookcoding@gmail.com>
2019-03-09 14:42:31 -0500

    cleaned up code format

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -3,77 +3,77 @@ import Modal from 'react-responsive-modal';
 import './modal.css'
 
 export default class CustomContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          open: false,
-        };
-        this.myRef = React.createRef();
-      }
-  
-    handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        event.preventDefault();
-      }
-
-    onOpenModal = () => {
-      this.setState({ open: true });
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
     };
-  
-    onCloseModal = () => {
-      this.setState({ open: false });
-    };
-  
-    render() {
-      const { open } = this.state;
-      return (
-        <div className="signup">
-          <div ref={this.myRef} />
-          <button className="btn btn-action" onClick={this.onOpenModal}>
-            Sign Up
-          </button>{' '}
-          {/* <a
-            href="/profile:id"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            See source code
-          </a> */}
-          <Modal
-            open={open}
-            onClose={this.onCloseModal}
-            center
-            container={this.myRef.current}
-          >
-            <form onSubmit={this.handleSubmit}>
-            <h4>Sign Up</h4>
-            <label className='modalForm'>
-                First Name: 
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-                Last Name: 
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-                <br />
-                Username:
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-                Password:
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-                <br />
-                Email:
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
-
-            </label>
-            <br />
-            <input type="submit" value="Submit" />
-              
-            </form>
-          </Modal>
-        </div>
-      );
-    }
+    this.myRef = React.createRef();
   }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+    return (
+      <div className="signup">
+        <div ref={this.myRef} />
+        <button className="btn btn-action" onClick={this.onOpenModal}>
+          Sign Up
+        </button>{' '}
+        {/* <a
+          href="/profile:id"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See source code
+        </a> */}
+        <Modal
+          open={open}
+          onClose={this.onCloseModal}
+          center
+          container={this.myRef.current}
+        >
+        <form onSubmit={this.handleSubmit}>
+          <h4>Sign Up</h4>
+          <label className='modalForm'>
+            First Name: 
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            Last Name: 
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <br />
+            Username:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            Password:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <br />
+            Email:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
+            
+        </form>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 //   import React, { Component } from 'react';
 // import Modal from 'react-responsive-modal';
