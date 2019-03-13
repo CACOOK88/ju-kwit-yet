8970e5981639351e49c01fd209231c8f24ac2f8b
Aijune <43250155+AFilippis@users.noreply.github.com>
2019-03-12 21:41:34 -0400

    renamed images
    still messing with carousel

--- client/src/components/HabitSlide/content.js
+++ client/src/components/HabitSlide/content.js
@@ -1,27 +1,27 @@
 export default [
 	{
     title: 'Be Active',	
-    image: '../Images/activity-athlete-barbell-703009.jpg',
+    image: '../Images/be-active.jpg',
     name: 'Weights'
     },
     {
     title: 'Stop Smoking',		
-    image: '../Images/addiction-ashtray-blur-247040.jpg',
+    image: '../Images/stop-smoking.jpg',
     name: 'No Smoking'
     },
     {
     title: 'Talk to Someone New',		
-    image: '../Images/backlit-bright-dawn-697243.jpg',
+    image: '../Images/talk-new.jpg',
     name: 'Friends'
     },
     {
     title: 'Drink More Water',	
-    image: '../Images/blur-bottle-boy-1126557.jpg',
+    image: '../Images/drink-more-water.jpg',
     name: 'Water'
     },
     {
     title: 'Eat More Avocado Toast',		
-    image: '../Images/appetizer-avocado-bread-566566.jpg',
+    image: '../Images/avocado-toast.jpg',
     name: 'Toast'
     }
 ]
