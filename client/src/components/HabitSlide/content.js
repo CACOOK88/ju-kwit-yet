ffa59552d891b5e5409a2ace9aac3b454744438e
Chad Cook <chadcookcoding@gmail.com>
2019-03-13 16:01:20 -0400

    imported images properly and applied to slider map function for rendering

--- client/src/components/HabitSlide/content.js
+++ client/src/components/HabitSlide/content.js
@@ -1,27 +1,33 @@
+import beActive from '../Images/be-active.jpg'
+import stopSmoking from '../Images/stop-smoking.jpg'
+import talkNew from '../Images/talk-new.jpg'
+import drinkWater from '../Images/drink-more-water.jpg'
+import avocadoToast from '../Images/avocado-toast.jpg'
+
 export default [
 	{
     title: 'Be Active',	
-    image: '../Images/be-active.jpg',
+    image: beActive,
     name: 'Weights'
     },
     {
     title: 'Stop Smoking',		
-    image: '../Images/stop-smoking.jpg',
+    image: stopSmoking,
     name: 'No Smoking'
     },
     {
     title: 'Talk to Someone New',		
-    image: '../Images/talk-new.jpg',
+    image: talkNew,
     name: 'Friends'
     },
     {
     title: 'Drink More Water',	
-    image: '../Images/drink-more-water.jpg',
+    image: drinkWater,
     name: 'Water'
     },
     {
     title: 'Eat More Avocado Toast',		
-    image: '../Images/avocado-toast.jpg',
+    image: avocadoToast,
     name: 'Toast'
     }
 ]
--- client/src/components/HabitSlide/index.js
+++ client/src/components/HabitSlide/index.js
@@ -5,17 +5,17 @@ import habitSlide from './habitSlide.css';
 
 function HabitSlide() {
 	return (
-		<div>
+		<div className="habitSlideContainer">
 			{/* <h2>HabitSlide</h2> */}
 			<Slider classNames={habitSlide} autoplay={4000}>
 				{content.map((item, index) => (
 					<div key={index}>   
                     {/* what the code was 
                     style={{ background: `url('${item.image}') no-repeat center center` }}    */}              
-                      <img src={item.image} alt={item.name} /> 
-						<div className="center">
+                      <img src={item.image} alt={item.name} className="sliderImageContent" /> 
+						{/* <div className="center">
 							<h1>{item.title}</h1>								
-						</div>
+						</div> */}
 					</div>
 				))}
 			</Slider>
